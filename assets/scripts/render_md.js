6a7e069364b3d4c41bbfd00919bd28322cb322ae
macfly <manuel@webdesignord.fr>
2018-04-17 09:17:04 +0100

    maj a/s/rend_md.js > nettoyage + add fct td_get_deadline_(tab_tr, td_idx) **reusable**

--- assets/scripts/render_md.js
+++ assets/scripts/render_md.js
@@ -1,43 +1,22 @@
 marked.setOptions({});
 
 var monmd = '';
-var url   = "file:///C:/Users/mdevries/Desktop/dl2018.md"
-var url   = "assets/dl2018.md"
 var url   = "./dl2018.md"
 
 function reqListener() {
   monmd = this.responseText;
-  // console.log(monmd)
   document.getElementById('content').innerHTML = marked(monmd);
   
 
-  // add fct on btn deadline
-  var html  = document.body.innerHTML,
-      dates = html.match(/[0-9]{2}\/[0-9]{2}\/(17|18)/ig);
-      regDates = /[0-9]{2}\/[0-9]{2}\/(17|18)/ig;
 
-  function insertAfter(el, referenceNode) {
-      referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
-  }
-  function insertBefore(el, referenceNode) {
-      referenceNode.parentNode.insertBefore(el, referenceNode);
-  }
 
-  var btn = document.createElement('button');
-  btn.innerHTML = 'deadline';
-  // btn.onclick = function () {
-  //   console.log(dates, html);
-  // }
-  
-  // inject deadline btn
-  const ref = document.querySelector('#tl-id');
-  insertBefore(btn, ref);
-
-  // get tabel tr td[3]
-  const table_row = document.querySelectorAll("table tr");
-  let td_deadline = []
+  // get table tr td[3]
+  const table_row     = document.querySelectorAll("table tr");
+  let regDates        = /[0-9]{2}\/[0-9]{2}\/(17|18)/ig;
+  let td_deadline     = [];
   let td_deadlinetest = []
   let td_deadline_txt = [];
+
   const td_get_deadline = () => {
 
     for (let tr of table_row) {
@@ -49,23 +28,38 @@ function reqListener() {
         td_deadline.push(data_date);
         data_date.style.backgroundColor = "#FFFF00";
         td_deadline_txt.push(data_date.textContent);
-        td_deadlinetest = Array.from(data_date);
+        // td_deadlinetest = Array.from(data_date);
         idx++;
       }
     }
     // return td_deadline;
   };
   td_get_deadline();
-  // console.log(td_deadline, td_deadline[0].textContent, td_deadline_txt[0].split('/'));
-  function toObject(arr) {
-    var rv = {};
-    for (var i = 0; i < arr.length; ++i)
-      rv[i] = arr[i];
-    return rv;
+  
+  const td_get_deadline_ = (tab_tr,td_idx) => {
+    const cel = [], txt = [];
+    for (let tr of tab_tr) {
+      const data_date = tr.querySelectorAll('td')[td_idx];
+
+      if (data_date && data_date.textContent.match(regDates) ){
+        var idx = 0;
+        cel.push(data_date);
+        txt.push(data_date.textContent)
+        // console.log(data_date, data_date.textContent);
+        idx++;
+      }
+    }
+    // retourne un tableau[tdHtmlCol,tdHtmlCol.txt] // console.log(cel, txt)
+    return [cel, txt];
   };
-  // console.log(toObject(td_deadline));
-  const objTd = toObject(td_deadline);
-  // console.log(objTd)
+  // td_get_deadline_(table_row,8);
+  // retour des cellules :
+  console.log(td_get_deadline_(table_row,3)[0]);
+  // retour des cellules.txt :
+  console.log(td_get_deadline_(table_row,3)[1]);
+  // console.log(td_deadline, td_deadline[0].textContent, td_deadline_txt[0].split('/'));
+
+
   // création tableaux de dates en ordre décroissantes  
   const arry_sort = td_deadline_txt.sort(function(a, b){
     var aa = a.split('/').reverse().join(),
@@ -78,28 +72,27 @@ function reqListener() {
       return self.indexOf(value) === index;
   }
   const arry_date_unique = arry_sort.filter(onlyUnique);
+  /*
   console.log('td_deadline: ', td_deadline );
   console.log('arry_sort: ', arry_sort);
   console.log('arry_date_unique: ', arry_date_unique);
-  // console.log(td_deadline,arry_sort);
+  console.log(td_deadline,arry_sort);
+  */
   var cpt = 0;
   function getIdx(tab1, tab2) {
     function addIdx() {
       for (var i = 0; i < tab1.length; i++) {
         if( tab1[i].textContent === tab2[cpt] ) {
-          console.log('job:',cpt,tab1[i],tab1[i].textContent);
+          // console.log('job:',cpt,tab1[i],tab1[i].textContent);
           tab1[i].innerHTML += '<span class="job">' + (cpt + 1) + '</span>'; 
         }
       }
     }
 
-    // while(cpt < tab1.length && tab2[cpt]) {
     while(cpt < tab1.length) {
       // tab2.shift();
-      // alert(tab2[cpt]);
       addIdx();
       cpt++;
-      // console.log('while cpt: ' + cpt, `while tab2[cpt]: ${tab2[cpt]} `);
     }
   }
 
@@ -118,3 +111,32 @@ xobj.send();
     console.log(`txt: ${txt}, cellule: `);
     cpt++;
   }*/
+  /*
+  var html  = document.body.innerHTML,
+      dates = html.match(/[0-9]{2}\/[0-9]{2}\/(17|18)/ig);
+  */
+  /*function toObject(arr) {
+    var rv = {};
+    for (var i = 0; i < arr.length; ++i)
+      rv[i] = arr[i];
+    return rv;
+  };
+  // console.log(toObject(td_deadline));
+  const objTd = toObject(td_deadline);*/
+  
+  /*
+  function insertAfter(el, referenceNode) {
+      referenceNode.parentNode.insertBefore(el, referenceNode.nextSibling);
+  }
+  function insertBefore(el, referenceNode) {
+      referenceNode.parentNode.insertBefore(el, referenceNode);
+  }
+  var btn = document.createElement('button');
+  btn.innerHTML = 'deadline';
+  // btn.onclick = function () {
+  //   console.log(dates, html);
+  // }
+  // inject deadline btn
+  const ref = document.querySelector('#tl-id');
+  insertBefore(btn, ref);
+  */
