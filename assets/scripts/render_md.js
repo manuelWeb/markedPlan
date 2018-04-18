da87fe6e0dd8728b103fef7fd6d5751e84f44f50
manuelWeb <devries.webdesigner@gmail.com>
2018-04-18 20:39:54 +0200

    add class ok for job 1 in styles.css

--- assets/scripts/render_md.js
+++ assets/scripts/render_md.js
@@ -11,7 +11,7 @@ function reqListener() {
   const table_row = document.querySelectorAll("table tr");
   const regDates  = /[0-9]{2}\/[0-9]{2}\/(17|18)/ig;
   
-  const get_td = (tab_tr,td_idx) => {
+  const get_td = (tab_tr, td_idx, bgcolor) => {
     const cel = [], txt = [];
     for (let tr of tab_tr) {
       const data_date = tr.querySelectorAll('td')[td_idx];
@@ -21,7 +21,7 @@ function reqListener() {
         cel.push(data_date);
         txt.push(data_date.textContent);
 
-        data_date.style.backgroundColor = "#FFFF00";
+        data_date.style.backgroundColor = bgcolor;
         // console.log(data_date, data_date.textContent);
         idx++;
       }
@@ -33,10 +33,15 @@ function reqListener() {
   // retour des td + td.txt :
   // console.log( deadline_td );
   // console.log( deadline_txt );
-  const deadline_td = get_td(table_row,3)[0];
+  const deadline_td = get_td(table_row,3,"#FFFF00")[0];
   const deadline_txt = get_td(table_row,3)[1];
   const dead_sort = sortAryArg(deadline_txt);
   const dead_unique = dead_sort.filter(onlyUnique);
+
+  const date_n_td = get_td(table_row,8,"#FF9900")[0];
+  const date_n_txt = get_td(table_row,8)[1];
+  const date_n_sort = sortAryArg(date_n_txt);
+  const date_n_unique = date_n_sort.filter(onlyUnique);
 
   function sortAryArg (ary) {
     return ary.sort(function(a, b){
@@ -48,20 +53,19 @@ function reqListener() {
   }
 
   console.log( 'dead_sort:', dead_sort );
+  console.log( 'date_n_sort:', date_n_sort );
   
   function onlyUnique(value, index, self) { 
       return self.indexOf(value) === index;
   }
 
-
-
-  var cpt = 0;
-  function getIdx(tab1, tab2) {
+  function getIdx(tab1, tab2, classpan) {
+    var cpt = 0;
     function addIdx() {
       for (var i = 0; i < tab1.length; i++) {
         if( tab1[i].textContent === tab2[cpt] ) {
           // console.log('job:',cpt,tab1[i],tab1[i].textContent);
-          tab1[i].innerHTML += '<span class="job">' + (cpt + 1) + '</span>'; 
+          tab1[i].innerHTML += '<span class="' + classpan + '">' + (cpt + 1) + '</span>'; 
         }
       }
     }
@@ -73,7 +77,8 @@ function reqListener() {
     }
   }
 
-  getIdx(deadline_td, dead_unique);
+  getIdx(deadline_td, dead_unique, "job");
+  getIdx(date_n_td, date_n_unique, "daten");
 
 } // end reqListener
 
